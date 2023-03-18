c1245756f4a4224edf0cbb1eca53989ad6a3ded0
Zach Milby <zachary.milby@gmail.com>
2023-03-18 13:06:42 -0400

    Completed Icecream List

--- OneDrive - University of Louisville/Desktop/Code Louisville/Knowledge check 2/KnowledgeCheck2/KnowledgeCheck2/Icecream.cs
+++ OneDrive - University of Louisville/Desktop/Code Louisville/Knowledge check 2/KnowledgeCheck2/KnowledgeCheck2/Icecream.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace KnowledgeCheck2
 {
-    internal class Icecream
+    public class Icecream
     {
+        public string name;
+        public Flavor flavor;
+        
+        public string price;
+        public string review;
+    }
+
+    public class Flavor
+    {
+        public string name;
+        public Flavor(string name) { this.name = name; }
     }
 }
--- OneDrive - University of Louisville/Desktop/Code Louisville/Knowledge check 2/KnowledgeCheck2/KnowledgeCheck2/Program.cs
+++ OneDrive - University of Louisville/Desktop/Code Louisville/Knowledge check 2/KnowledgeCheck2/KnowledgeCheck2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using KnowledgeCheck2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var icecreamList = new List<Icecream>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var icecream = new Icecream();
+
+    Console.WriteLine("Enter an icecream flavor");
+    string icecreamFlavor = Console.ReadLine();
+    icecream.flavor = new Flavor(icecreamFlavor);
+
+    Console.WriteLine("Enter the price of " + icecream.flavor.name);
+    icecream.price = Console.ReadLine();
+
+    Console.WriteLine("Enter a review of " + icecream.flavor.name);
+    icecream.review = Console.ReadLine();
+
+    icecreamList.Add(icecream);
+}
+
+// Print out the list of records using Console.WriteLine()
+
+foreach (Icecream icecream in icecreamList)
+{
+    Console.WriteLine("Icecream: " + icecream.flavor.name + " Price: " + (icecream.price) + " Review: " + (icecream.review));
+}
